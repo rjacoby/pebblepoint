43f62ae032ced77782a34da45bd88286455fe321
Rafi Jacoby <rjacoby@astrokid.com>
2014-06-17 10:50:13 -0500

    untested JS

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,4 +1,5 @@
 var initialized = false;
+var config = {};
 
 Pebble.addEventListener("ready", function() {
   console.log("ready called!");
@@ -15,4 +16,37 @@ Pebble.addEventListener("webviewclosed", function(e) {
   // webview closed
   var options = JSON.parse(decodeURIComponent(e.response));
   console.log("Options = " + JSON.stringify(options));
+  config = options;
 });
+
+function sendPowerPointCommand(command, callback, errorCallback) {
+    var req = new XMLHttpRequest();
+    // TODO: test if we're configured
+    url = "http://" + config.address + ":" + config.port + "/go/" + command;
+    console.log("POSTing to: " + url);
+    req.open("POST", url, true);
+    req.responseType = "arraybuffer";
+    req.onload = function(e) {
+      console.log("loaded");
+      var body = req.response;
+      if(req.status == 200 && body) {
+        responseData = JSON.parse(body);
+        console.log("body", responseData);
+        callback(body); //???
+      }
+      else {
+        errorCallback("Request status is " + req.status);
+      }
+    };
+    req.onerror = function(e) {
+      errorCallback(e);
+    };
+    req.send(null);
+}
+
+Pebble.addEventListener("appmessage",
+  function(e) {
+    console.log("Received message: " + e.payload);
+    // TODO: call the send helper with a munged payload
+  }
+);
