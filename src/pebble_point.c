c8550cefb0332aaee243ee85cd4c6e221b8e35fb
Rafi Jacoby <rjacoby@astrokid.com>
2014-06-27 14:42:04 -0500

    centering initial prompt

--- src/pebble_point.c
+++ src/pebble_point.c
@@ -75,10 +75,11 @@ static void window_load(Window *window) {
   layer_add_child(window_layer, controls_layer);
 
   //*** GRect for the middle of screen
-  GRect mid_rect = (GRect) { .origin = {0, 40}, .size = { bounds.size.w, bounds.size.h - 60}};
+  GRect mid_rect = (GRect) { .origin = {0, 30}, .size = { bounds.size.w, bounds.size.h - 50}};
 
   //*** Prompt; can be replaced with status
-  prompt_layer = layer_create(mid_rect);
+  int prompt_height_offset = 50;
+  prompt_layer = layer_create(((GRect) { .origin = {0, prompt_height_offset}, .size = {mid_rect.size.w, mid_rect.size.h - prompt_height_offset}}));
   prompt_text_layer = text_layer_create(layer_get_bounds(prompt_layer));
   text_layer_set_text(prompt_text_layer, "Press for Prev/Next\n\nHold for First/Last");
   text_layer_set_text_alignment(prompt_text_layer, GTextAlignmentCenter);
@@ -89,12 +90,22 @@ static void window_load(Window *window) {
 
   //*** Status; starts out hidden
   status_layer = layer_create(mid_rect);
-  status_text_layer = text_layer_create(layer_get_bounds(status_layer));
+
+  GRect slide_index_rect = (GRect) {.origin = {0, 0}, .size = {mid_rect.size.w, mid_rect.size.h - 40}};
+  slide_index_text_layer = text_layer_create(slide_index_rect);
+  text_layer_set_text_alignment(slide_index_text_layer, GTextAlignmentCenter);
+  text_layer_set_font(slide_index_text_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+  text_layer_set_background_color(slide_index_text_layer, GColorBlack);
+  text_layer_set_text_color(slide_index_text_layer, GColorWhite);
+
+  status_text_layer = text_layer_create((GRect) {.origin = {0, (slide_index_rect.origin.y + slide_index_rect.size.h)}, .size = {mid_rect.size.w, (mid_rect.size.h - slide_index_rect.size.h)}});
   text_layer_set_text_alignment(status_text_layer, GTextAlignmentCenter);
+  text_layer_set_font(status_text_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
   text_layer_set_background_color(status_text_layer, GColorBlack);
   text_layer_set_text_color(status_text_layer, GColorWhite);
-  text_layer_set_font(status_text_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
+
   layer_add_child(status_layer, text_layer_get_layer(status_text_layer));
+  layer_add_child(status_layer, text_layer_get_layer(slide_index_text_layer));
   layer_add_child(window_layer, status_layer);
   layer_set_hidden(status_layer, true);
 }
@@ -133,12 +144,15 @@ void in_received_handler(DictionaryIterator *received, void *context) {
   Tuple *index_tuple = dict_find(received, AKEY_SLIDE_INDEX);
   Tuple *total_tuple = dict_find(received, AKEY_SLIDE_TOTAL);
   if (index_tuple && total_tuple){
-    static char message[16] = "";
-    snprintf(message, sizeof(message), "%u of %u", (unsigned int)index_tuple->value->uint32, (unsigned int)total_tuple->value->uint32);
+    static char index_message[4] = "";
+    static char total_message[16] = "";
+    snprintf(index_message, sizeof(index_message), "%u", (unsigned int)index_tuple->value->uint32);
+    snprintf(total_message, sizeof(total_message), "of %u", (unsigned int)total_tuple->value->uint32);
     // Works to logger
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Message: %s", message);
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Message: %s", total_message);
     // Doesn't work to Pebble screen
-    text_layer_set_text(status_text_layer, message);
+    text_layer_set_text(slide_index_text_layer, index_message);
+    text_layer_set_text(status_text_layer, total_message);
 
     layer_set_hidden(status_layer, false);
     layer_set_hidden(prompt_layer, true);
